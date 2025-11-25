e86e57df098245ba3e74c7af255e2c63c0c49833
kimkyungmin1704 <kimkyungmin1704@gmail.com>
2025-11-25 14:17:24 +0900

    재화상세표시 방법 변경
    누르고 있는 동안 - 팝업on/외부반경 터치로 닫기

--- Assets/_Project/Scripts/UI/UserInfo/TicketInfo.cs
+++ Assets/_Project/Scripts/UI/UserInfo/TicketInfo.cs
@@ -3,28 +3,44 @@ using UnityEngine.EventSystems;
 
 namespace CocoDoogy.UI.UserInfo
 {
-    public class TicketInfo : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    public class TicketInfo : MonoBehaviour, IPointerDownHandler
     {
         [SerializeField] private GameObject realTimeTicket;
+        // TODO : 터치 범위로 판정
+        //     - realTimeTicket 열려있을 떄 realTimeTicket 외의 다른 것을 터치했을 때 사라지게 
         
         private void Awake()
         {
-            realTimeTicket.SetActive(false);
+            if (realTimeTicket != null)
+                realTimeTicket.SetActive(false);
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            realTimeTicket.SetActive(true);
-        }
+            if (realTimeTicket == null)
+                return;
 
-        public void OnPointerUp(PointerEventData eventData)
-        {
-            realTimeTicket.SetActive(false);
-        }
+            bool willOpen = !realTimeTicket.activeSelf;
 
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            realTimeTicket.SetActive(false);
+            realTimeTicket.SetActive(willOpen);
+
+            if (willOpen)
+            {
+                // 팝업을 "현재 선택된 UI"로 지정
+                var target = realTimeTicket;
+
+                if (EventSystem.current != null)
+                    EventSystem.current.SetSelectedGameObject(target);
+            }
+            else
+            {
+                // 닫힐 때는 선택도 비워줌 (선택 관련 꼬임 방지용)
+                if (EventSystem.current != null &&
+                    EventSystem.current.currentSelectedGameObject == realTimeTicket)
+                {
+                    EventSystem.current.SetSelectedGameObject(null);
+                }
+            }
         }
     }
 }
